035389c0d2f208c66fc568446d8cbaf4796e0c49
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-06-02 17:34:09 +0900

    2025-06-02
    2025-06-02

---
+++ cplus/compile/compile.sh
@@ -0,0 +1,23 @@
+# ========================================================================
+# ============================== 컴파일 명령어 ==============================
+# =======================================================================
+g++ -std=c++17 -Wall -Wextra -o 14502_dfs 14502_dfs.cpp
+
+g++ -std=c++17 -Wall -Wextra -o 14502_bfs 14502_bfs.cpp
+
+
+g++ -std=c++17 -Wall -Wextra -o 1937 1937.cpp
+g++ -std=c++17 -Wall -Wextra -o 4963 4963.cpp
+g++ -std=c++17 -Wall -Wextra -o 4963_bfs 4963_bfs.cpp
+
+
+g++ -std=c++17 -Wall -Wextra -o 14503 14503.cpp
+
+g++ -std=c++17 -Wall -Wextra -o 2665 2665.cpp
+
+g++ -std=c++17 -Wall -Wextra -o 2665_dijkstra 2665_dijkstra.cpp
+
+
+g++ -std=c++17 -Wall -Wextra -o 2468 2468.cpp
+
+g++ -std=c++17 -Wall -Wextra -o 2468 2468.cpp
