a4c786474619fbae2dfb1402ffd696e96c2e0fda
sexyseunghwan <ssh9308@naver.com>
2025-07-28 22:22:02 +0900

    2025-07-28
    2025-07-28

--- cplus/concept/dynamic.cpp
+++ cplus/concept/dynamic.cpp
@@ -4,13 +4,51 @@
 using namespace std;
 
 // Bottom-up
-int fibonacci(int n) {
+// int fibonacci(int n) {
+//     vector<int> dp(n+1);
+
+//     dp[0] = 0;
+//     dp[1] = 1;
+
+//     for (int i = 2; i <= n; i++) 
+//     {
+//         dp[i] = dp[i-1] + dp[i-2];
+//     }
+
+//     return dp[n];
+// }
+
+// int dp_mat[100];
+// bool visited[100];
+
+// // Top-down
+// int fib(int n) 
+// {
+//     if (n <= 1) return n;
+//     if (visited[n]) return dp_mat[n];
+//     visited[n] = true;
+//     return dp_mat[n] = dp_mat[n-1] + dp_mat[n-2];
+    
+// }
+
+vector<int> dp(100, -1);
+
+int fibonacci_top_down(int n)
+{
+    if (n <= 1) return n;
+    if (dp[n] != -1) return dp[n]; /* 메모이제이션 */ 
+    return dp[n] = fibonacci_top_down(n-1) + fibonacci_top_down(n-2);
+}
+
+
+int fibonacci_bottom_up(int n)
+{
     vector<int> dp(n+1);
 
     dp[0] = 0;
     dp[1] = 1;
 
-    for (int i = 2; i <= n; i++) 
+    for (int i = 2; i <= n; i++)
     {
         dp[i] = dp[i-1] + dp[i-2];
     }
@@ -18,28 +56,13 @@ int fibonacci(int n) {
     return dp[n];
 }
 
-int dp_mat[100];
-bool visited[100];
-
-// Top-down
-int fib(int n) 
-{
-    if (n <= 1) return n;
-    if (visited[n]) return dp_mat[n];
-    visited[n] = true;
-    return dp_mat[n] = dp_mat[n-1] + dp_mat[n-2];
-    
-}
-
-
 int main()
 {
     
     int n;
     cin >> n;
 
-    cout << n << " : " << fibonacci(n) << "" << endl;
-
+    cout << n << " : " << fibonacci_bottom_up(n) << "" << endl;
 
     return 0;
 }
