41ea91b86f6c0e585cc66d833366b1225c530991
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-12-10 11:55:19 +0900

    2025-12-10
    2025-12-10

---
+++ test/App.java
@@ -0,0 +1,8 @@
+package test;
+
+public class App {
+    public static void main(String[] args) throws Exception {    
+        //TwoString.test();
+        Anagram.test();
+    }
+}
---
+++ test/TwoString.java
@@ -0,0 +1,78 @@
+package test;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+    */
+    public static String twoStrings(String s1, String s2) {
+        // Write your code here
+        //Map<String, Integer> map = new HashMap<>();
+        Set<String> haSet = new HashSet<>();
+
+        String[] s1Split = s1.split("");
+        String[] s2Split = s2.split("");
+
+        for (String elem : s1Split) {
+            haSet.add(elem);           
+        }
+
+        boolean flag = false;
+
+        for (String elem : s2Split) {
+            if (haSet.contains(elem)) {
+                flag = true;
+                break;
+            }
+        }
+
+        return flag == true ? "YES" : "NO";
+    }
+
+}
+
+public class TwoString {
+    
+    public static void test() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+                //String result = "test";
+                
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
